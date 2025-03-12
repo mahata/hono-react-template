2af30e2db86a6f3e8c7dc350e887e0157d090f15
Yasunori MAHATA <mahata777@gmail.com>
2025-03-12 18:31:07 +0900

    Replace Top component with App component in main rendering

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,12 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from "react-router";
 import './index.css'
-import Top from './Top.tsx'
+import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
-      <Top />
+      <App />
     </BrowserRouter>
   </StrictMode>,
 )
