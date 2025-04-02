7c9b7fb413855be6462304b898f796aa2d9bae83
Yasunori MAHATA <mahata777@gmail.com>
2025-04-02 15:05:33 +0900

    🚨 Add type definitions for Hono app and initialize environment schema

---
+++ backend/env.ts
@@ -0,0 +1,1 @@
+const EnvSchema = z.object()
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,8 +1,12 @@
-import { Hono } from 'hono'
+import { Hono } from 'hono';
 import { cors } from 'hono/cors';
 import books from './routes/books';
 
-const app = new Hono()
+type Bindings = {
+  ASSETS: { fetch: (req: Request | URL) => Promise<Response> }
+}
+
+const app = new Hono<{ Bindings: Bindings }>()
 
 app.use('/api/*', cors({
   origin: 'http://localhost:5173',
